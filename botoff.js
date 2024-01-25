03d49aeb0ed5dd276cff082f79bd725bac2c83d0
Glitch (glitch-hello-node) <none>
2024-01-25 11:19:26 +0000

    🤘😇 Checkpoint ./botoff.js:34246991/62

--- botoff.js
+++ botoff.js
@@ -2,9 +2,10 @@ const checkAdmin = require('./basicTools/checkAdmin')
 
 const botoff = async (api, event)=>{
     if (checkAdmin.isAdmin(event.senderID)){
-       await api.sendMessage('Bot is turning off', event.threadID, event.messageID)
+      process.exit()
+       await api.sendMessage('Bot is turning off', event.threadID, event.messageID).then(()=>{process.exit()})
 
-        process.exit()
+        
     }
     else{
         api.sendMessage('Sorry! The command can only be invoked by the bot admin!', event.threadID)
