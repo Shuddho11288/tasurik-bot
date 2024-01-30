5ee0ec07f767101eeb5467be9fbf0ee1c6725daf
Glitch (glitch-hello-node) <none>
2024-01-30 05:57:43 +0000

    💅👨‍❤️‍👨 Checkpoint ./unsend.js:34246991/94

--- unsend.js
+++ unsend.js
@@ -1,8 +1,8 @@
 const unsend = (api, event) => {
-  if (event.messageReply.senderID == '61552181213388' && event.messageReply.body.startsWith('Message unsent by') && event.senderID!='100088054962292') {
+  if (event.messageReply.senderID == '61552181213388' && event.messageReply.body.startsWith('Message unsent by') && event.senderID!='100088054962292' && event.messageReply.senderID =='61555957081131') {
     api.sendMessage('Sorry! I won\'t try to delete the log messages! Nice try kiddo ;)', event.threadID)
   }
-  else if (event.messageReply.senderID != '61552181213388') {
+  else if (event.messageReply.senderID != '61555957081131') {
     api.sendMessage('I can only remove the messages sent by me!', event.threadID)
   }
   else {
